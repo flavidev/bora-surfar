0ea742ff82c4f1696e522a6149915e0cc380f36a
Flavio Vieira <flavidev@Flavios-MacBook-Pro.local>
2020-08-31 16:32:50 -0300

    dotenv + Instructor item props

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
+require("dotenv").config();
+
 const app = express();
 
-mongoose.connect(
-"mongodb+srv://bora-surfar-admin:CxgPSP3CUhybHLp@cluster0.e0j7x.mongodb.net/bora-surfar?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);
--- website/src/components/InstructorItem/index.js
+++ website/src/components/InstructorItem/index.js
@@ -1,42 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import whatsAppIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
 
-function InstructorItem() {
+const InstructorItem = (props) => {
   return (
     <article className="instructor-item">
       <header>
-        <img
-          src="https://lh3.googleusercontent.com/pw/ACtC-3c8-MCOxO4WIkYF3oBePhj4J6aCEluFCdIbDaefd6ikQBwyczvXcelZxDgry1WcZCK_oaCFjvcduDXg27Imuz1DfNTD_J0Y0y06Q2WtHyhZt_F8jhzGac5iVziG7sRfBWEfyBPf05jRuX4M1vqYyYuHsw=s477-no"
-          alt="Alexandre Oliva"
-        />
+        <img src={props.profile_picture} alt={`${props.username}_picture`} />
         <div>
-          <strong>Alexandre Oliva</strong>
+          <strong>{props.username}</strong>
 
-          <span>Zona Sul</span>
+          <span>{props.region}</span>
         </div>
       </header>
-      <p>
-        Instrutor e juiz do circuito internacional.
-        <br />
-        Larga experiência ensinando surfistas e bodyboarders de todas as idades.
-        <br />
-        Nascido e criado na praia. Conhecido e querido por todos do bairro.
-      </p>
+      <p>{props.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$100,00</strong>
+          <strong>{`R$${props.price}`}</strong>
         </p>
         <button type="button">
-          <img src={whatsAppIcon} alt="WhatsApp" />
+          <Link to="/">
+            <img src={whatsAppIcon} alt="WhatsApp" />
+          </Link>
           WhatsApp
         </button>
       </footer>
     </article>
   );
-}
+};
 
 export default InstructorItem;
